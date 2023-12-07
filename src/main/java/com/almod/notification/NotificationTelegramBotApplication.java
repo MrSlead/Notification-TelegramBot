9e13d1e0cce5a05d58779b82f2e97886d332753c
Alex <mordyasov.60@mail.ru>
2023-12-08 00:25:17 +0700

    The project was created

---
+++ src/main/java/com/almod/notification/NotificationTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.almod.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationTelegramBotApplication.class, args);
+	}
+
+}
